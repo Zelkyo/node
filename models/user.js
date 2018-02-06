0b4a916766c3d5460e1b862a09616c27aa8ffbb6
DESKTOP-HUHQTDD\Axel De Sutter <axel@skyfight.be>
2018-02-06 18:19:06 +0100

    Ajout de la méthode 'connect' et création de la session utilisateur

--- models/user.js
+++ models/user.js
@@ -1,17 +1,52 @@
 let connection = require('../config/database.js');
+let md5 = require('md5');
 
 class User{
     
-    static register(data, callback) {
-        if(data.firstname && data.surname && data.pseudo && data.email && data.password && data.password2 && data.cgu){
-            if(data.pseudo.indexOf(' ') !== -1){
-                if(data.password == data.password2){
-                    callback('ok');
+    // User loging in
+    static login(data,callback) {
+        if(data.email && data.password){
+            connection.query('SELECT * FROM users WHERE email = ? AND password = ?', [data.email, md5(data.password)], (err,rows) => {
+                if (err) throw err;
+                if(rows.length > 0){
+                    callback('connected', rows[0])
                 }else{
-                    callback('Les mots de passe ne correspondent pas');
+                    callback('Identifiants incorrects');
                 }
+            })
+        }else{
+            callback('Merci de compléter tous les champs');
+        }
+    }
+
+    // User registration
+    static register(data, callback) {
+        if(data.firstname && data.surname && data.pseudo.replace(/\s/g,'') && data.email && data.password && data.password2){
+            if(data.cgu){
+                    if(data.password == data.password2){
+                        connection.query('SELECT * FROM users WHERE pseudo = ?', [data.pseudo.replace(/\s/g,'')], (err,rows) => {
+                            if (err) throw err;
+                            if(rows.length == 0){
+                                connection.query('SELECT * FROM users WHERE email = ?', [data.email], (err,rows) => {
+                                    if (err) throw err;
+                                    if(rows.length == 0){
+                                        connection.query('INSERT INTO users (pseudo,firstname,surname,email,password,category) VALUES (?,?,?,?,?,?)', [data.pseudo.replace(/\s/g, ''), data.firstname, data.surname, data.email, md5(data.password), data.category], (err, rows) => {
+                                            if (err) throw err;
+                                            callback('registered', rows);
+                                        })
+                                    }else{
+                                        callback('Désolé, cette adresse e-mail est déjà utilisée');
+                                    }
+                                })
+                            }else{
+                                callback('Désolé, ce pseudonyme n\'est plus disponible');
+                            }
+                        })
+                    }else{
+                        callback('Les mots de passe ne correspondent pas');
+                    }
             }else{
-                callback('Votre pseudo ne peut pas contenir d\'espace');
+                callback('Vous devez accepter les Conditions Générales d\'Utilisation');
             }
         }else{
             callback('Merci de compléter tous les champs');
--- server.js
+++ server.js
@@ -18,6 +18,15 @@ app.use(session({
 }))
 app.use(require('./middlewares/flash.js'));
 
+// Main page
+app.get('/', (req,res) => {
+    if(req.session.userId !== '' && req.session.userId !== undefined){
+        res.redirect('/'+req.session.userId);
+    }else{
+        res.redirect('/connect');
+    }
+})
+
 // Registration
 app.get('/register', (req,res) => {
     if(req.body){
@@ -29,14 +38,42 @@ app.get('/register', (req,res) => {
 
 app.post('/register', (req,res) => {
     let User = require('./models/user.js');
-    User.register(req.body, (response) => {
-        if(response == 'registered'){
-            res.render('pages/register');
+    User.register(req.body, (response, data) => {
+        if(response === 'registered'){
+            res.render('pages/connect', {success: 'Vous pouvez vous connecter !'});
         }else{
             req.flash('error', response)
-            res.render('pages/register');
+            res.redirect('/register');
         }
     })
+})
+
+// Connection
+app.get('/connect', (req,res) => {
+    res.render('pages/connect');
+})
+
+app.post('/connect', (req,res) => {
+    let User = require('./models/user.js');
+    User.login(req.body, (response, data) => {
+        if(response === 'connected'){
+            req.session.userId = data.id;
+            req.session.firstname = data.firstname;
+            req.session.surname = data.surname;
+            req.session.points = data.points;
+            req.session.registerDate = data.creation;
+            res.redirect('/');
+        }else{
+            req.flash('error', response);
+            res.redirect('/connect');
+        }
+    })
+})
+
+// Disconnection
+app.get('/disconnect', (req,res) => {
+    req.session.id = undefined;
+    res.redirect('/connect');
 })
 
 app.listen(8080);
